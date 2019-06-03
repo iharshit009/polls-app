0941830d79acfbf63ccae26d380299887fd45899
HARSHIT JAIN <44135316+iharshit009@users.noreply.github.com>
2019-06-03 23:43:09 +0530

    Add files via upload

---
+++ polls/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+from . import views
+
+app_name='polls'
+urlpatterns = [
+   # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
